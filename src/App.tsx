f82711e78cc72e222e84269e4f367c7fce32ae69
simarjot <simarjot0032@gmail.com>
2023-10-24 21:06:05 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Menubar from "./components/Menubar";
 import Books from "./components/Books";
+import Classten from "./components/Classten";
+import Classnine from "./components/Classnine";
 function App() {
   return (
     <>
@@ -22,6 +24,9 @@ function App() {
           <Route path="/Samplepapers" element={<Home></Home>}></Route>
 
           <Route path="/Assignments" element={<Home></Home>}></Route>
+          <Route path="/Classnine" element={<Classnine></Classnine>}></Route>
+
+          <Route path="/Classten" element={<Classten></Classten>}></Route>
         </Routes>
       </BrowserRouter>
     </>
