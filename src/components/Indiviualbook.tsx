0b1286c1db59200f766d85fa63c0103bdd85fad0
simarjot <simarjot0032@gmail.com>
2023-10-27 00:29:25 +0530

    updat

--- src/components/Indiviualbook.tsx
+++ src/components/Indiviualbook.tsx
@@ -1,1 +1,36 @@
 // component for each books
+import "../styles/Classtenbooks.css";
+interface Props {
+  linkitem: any;
+  numberofchapter: number;
+  chaptername: string;
+}
+export default function Indiviualbook({
+  linkitem,
+  numberofchapter,
+  chaptername,
+}: Props) {
+  return (
+    <>
+      <hr className="bookshr" />
+      <div className="bookslist">
+        <div className="indiviualbook">
+          <div className="leftsidelist">
+            <div className="contentleftside">{numberofchapter}</div>
+          </div>
+          <div className="rightsidelist">
+            <div className="upperright">{chaptername}</div>
+            <div className="lowerright">
+              <a href={linkitem} className="linktobook" target="_blank">
+                <button className="btnbook">View Online</button>
+              </a>
+              <a href={linkitem} className="linktobook" download={linkitem}>
+                <button className="btnbook">Download</button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
